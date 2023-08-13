362a7ffc3600a79a46a25040e946118afac4331c
angelo-oviedo <angelooviedomont22@gmail.com>
2023-08-13 12:12:05 -0600

    GCS upload DAG

--- dags/retail.py
+++ dags/retail.py
@@ -1,5 +1,10 @@
+# General imports for Airflow
 from airflow.decorators import dag, task
 from datetime import datetime, timedelta
+
+# Utility imports
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 
 @dag(
     
@@ -10,5 +15,22 @@ from datetime import datetime, timedelta
 )
 
 def retail():
+    
+    # This task will upload the csv file to GCS bucket
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_csv_to_gcs',
+        src='include/dataset/online_retail.csv',
+        dst='raw/online_retail.csv',
+        bucket='dataset_online_retail',
+        gcp_conn_id='gcp',
+        mime_type='text/csv',
+    )
+    
+    # This task will create a dataset in BigQuery
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
     
 retail()
