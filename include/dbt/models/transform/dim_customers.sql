aef8656f538210efd727ef32c6a776b269c026a2
angelo-oviedo <angelooviedomont22@gmail.com>
2023-08-13 22:41:07 -0600

    Models

--- include/dbt/models/transform/dim_customers.sql
+++ include/dbt/models/transform/dim_customers.sql
@@ -1,8 +1,12 @@
-SELECT DISTINCT
-    {{ dbt_utils.generate_surrogate_key(['StockCode', 'Description', 'UnitPrice']) }} as product_id,
-		StockCode AS stock_code,
-    Description AS description,
-    UnitPrice AS price
-FROM {{ source('retail', 'raw_invoices') }}
-WHERE StockCode IS NOT NULL
-AND UnitPrice > 0
+WITH customer_cte AS (
+	SELECT DISTINCT
+	    {{ dbt_utils.generate_surrogate_key(['CustomerID', 'Country']) }} as customer_id,
+	    Country AS country
+	FROM {{ source('retail', 'raw_invoices') }}
+	WHERE CustomerID IS NOT NULL
+)
+SELECT
+    t.*,
+	cm.iso
+FROM customer_cte t
+LEFT JOIN {{ source('retail', 'country') }} cm ON t.country = cm.nicename
