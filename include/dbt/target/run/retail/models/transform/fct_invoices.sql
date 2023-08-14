7f4ed49289fcccf3e0ec32989af05c272ca5af24
angelo-oviedo <angelooviedomont22@gmail.com>
2023-08-13 22:46:39 -0600

    Ready to make the reporting models

--- include/dbt/target/run/retail/models/transform/fct_invoices.sql
+++ include/dbt/target/run/retail/models/transform/fct_invoices.sql
@@ -8,17 +8,27 @@
 
     OPTIONS()
     as (
-      WITH customer_cte AS (
-	SELECT DISTINCT
-	    to_hex(md5(cast(coalesce(cast(CustomerID as STRING), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(Country as STRING), '_dbt_utils_surrogate_key_null_') as STRING))) as customer_id,
-	    Country AS country
-	FROM `airflow-project-395816`.`retail`.`raw_invoices`
-	WHERE CustomerID IS NOT NULL
+      WITH fct_invoices_cte AS (
+    SELECT
+        InvoiceNo AS invoice_id,
+        InvoiceDate AS datetime_id,
+        to_hex(md5(cast(coalesce(cast(StockCode as STRING), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(Description as STRING), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(UnitPrice as STRING), '_dbt_utils_surrogate_key_null_') as STRING))) as product_id,
+        to_hex(md5(cast(coalesce(cast(CustomerID as STRING), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(Country as STRING), '_dbt_utils_surrogate_key_null_') as STRING))) as customer_id,
+        Quantity AS quantity,
+        Quantity * UnitPrice AS total
+    FROM `airflow-project-395816`.`retail`.`raw_invoices`
+    WHERE Quantity > 0
 )
 SELECT
-    t.*,
-	cm.iso
-FROM customer_cte t
-LEFT JOIN `airflow-project-395816`.`retail`.`country` cm ON t.country = cm.nicename
+    invoice_id,
+    dt.datetime_id,
+    dp.product_id,
+    dc.customer_id,
+    quantity,
+    total
+FROM fct_invoices_cte fi
+INNER JOIN `airflow-project-395816`.`retail`.`dim_datetime` dt ON fi.datetime_id = dt.datetime_id
+INNER JOIN `airflow-project-395816`.`retail`.`dim_product` dp ON fi.product_id = dp.product_id
+INNER JOIN `airflow-project-395816`.`retail`.`dim_customer` dc ON fi.customer_id = dc.customer_id
     );
   
