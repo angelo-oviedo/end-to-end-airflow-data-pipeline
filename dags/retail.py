1640921130cea593e839cb0ccc0132c4dac21d8c
angelo-oviedo <angelooviedomont22@gmail.com>
2023-08-13 19:00:53 -0600

    DAG retail configs

--- dags/retail.py
+++ dags/retail.py
@@ -5,6 +5,10 @@ from datetime import datetime, timedelta
 # Utility imports
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
 
 @dag(
     
@@ -26,11 +30,27 @@ def retail():
         mime_type='text/csv',
     )
     
-    # This task will create a dataset in BigQuery
     create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
         task_id='create_retail_dataset',
         dataset_id='retail',
         gcp_conn_id='gcp',
     )
+    
+    gcs_to_raw = aql.load_file(
+        task_id='gcs_to_raw',
+        input_file=File(
+            'gs://airflow project/raw/online_retail.csv',
+            conn_id='gcp',
+            file_type=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_supported=False,
+    )
+    
+    
     
 retail()
